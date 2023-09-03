29f16119ccbe776910242fa6fa33c53befb3c980
yrzpc <sec20ec044@sairamtap.edu.in>
2023-09-03 08:48:41 +0530

    latest commit

--- app/src/main/java/com/example/repairbrain20/BootCompleted.java
+++ app/src/main/java/com/example/repairbrain20/BootCompleted.java
@@ -1,12 +1,36 @@
 package com.example.repairbrain20;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class BootCompleted extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED"))
+        {
+            Log.e("uruttu_boot","booted");
 
+            AppSettings settings = new AppSettings(context);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.HOUR_OF_DAY, settings.getHour());
+            calendar.set(Calendar.MINUTE, settings.getMinute());
+
+            AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+            Intent alarm_intent = new Intent(context, AlarmReceiver.class);
+            PendingIntent alarm_pending = PendingIntent.getBroadcast(context, 100, alarm_intent, PendingIntent.FLAG_MUTABLE);
+
+            manager.setInexactRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarm_pending);
+        }
     }
 }
